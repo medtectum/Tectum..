05a31ae959e1d8abfcc11609f80f564dc61b6c25
medtectum <medtectum@gmail.com>
2026-09-14 22:12:44 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>TECTUM</h1>
+
+      <h2>Learn. Share. Connect.</h2>
+
+      <p>
+        A social learning platform for medical students.
+      </p>
+
+      <button>Get Started</button>
+
+      <br />
+      <br />
+
+      <button>Log In</button>
+    </main>
+  );
+}
